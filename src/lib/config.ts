cf8b812dceaa527d1d55a475d01d28da71750027
K SK <krishnask785@gmail.com>
2026-01-26 13:14:48 +0000

    update the api key to this AIzaSyB8t6kGf0OsJAM21I6esqlXAe6ayGlqneg

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,4 +9,4 @@ export const firebaseConfig: FirebaseOptions = {
   appId: "1:728995885616:web:b1a65d8d571ad68f7ab3f5",
 };
 
-export const geminiApiKey = "REPLACE_WITH_YOUR_GEMINI_API_KEY";
+export const geminiApiKey = "AIzaSyB8t6kGf0OsJAM21I6esqlXAe6ayGlqneg";
